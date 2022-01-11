f190b4a10ae0ce48864ace17daa48ae7c815d285
t-liwen.zhao <liwen.zhao@pcitc.com>
2022-01-11 17:07:41 +0800

    no message

--- src/main/java/com/coc/cocserver/CocServerApplication.java
+++ src/main/java/com/coc/cocserver/CocServerApplication.java
@@ -2,10 +2,11 @@ package com.coc.cocserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CocServerApplication {
 
     public static void main(String[] args) {
